64ee34cdafe435c5424a7f5ead4ea32b6f0dc3a1
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-07 17:56:30 -0500

    feat: resources implemented

---
+++ ChromaComics/Comics/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ChromaComics.Comics.Domain.Models;
+using ChromaComics.Comics.Resources;
+
+namespace ChromaComics.Comics.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveCategoryResource, Category>();
+        CreateMap<SaveComicResource, Comic>();
+    }
+}
