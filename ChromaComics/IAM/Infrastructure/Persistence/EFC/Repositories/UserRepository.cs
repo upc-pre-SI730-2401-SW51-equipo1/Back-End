f3a73977496be16a15b088b097b1048365e95ebe
GoodFries04 <114363212+GoodFries04@users.noreply.github.com>
2024-06-08 02:53:50 -0500

    feat: added "full" infrastructure layer

---
+++ ChromaComics/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+using ChromaComics.IAM.Domain.Model.Aggregates;
+using ChromaComics.IAM.Domain.Repositories;
+using ChromaComics.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ChromaComics.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.LearningCenterPlatform.API.IAM.Infrastructure.Persistence.EFC.Repositories;
+
+/**
+ * <summary>
+ *     The user repository
+ * </summary>
+ * <remarks>
+ *     This repository is used to manage users
+ * </remarks>
+ */
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
+{
+    /**
+     * <summary>
+     *     Find a user by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> FindByUsernameAsync(string username)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
+    }
+
+    /**
+     * <summary>
+     *     Check if a user exists by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>True if the user exists, false otherwise</returns>
+     */
+    public bool ExistsByUsername(string username)
+    {
+        return Context.Set<User>().Any(user => user.Username.Equals(username));
+    }
+}
