6f97e1113b0a7a0b199f69b72f507497c6b6e4f0
Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>
2024-06-08 03:57:15 -0500

    Add files via upload
    Signed-off-by: Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>

---
+++ ChromaComics/Recommendations/Domain/Repositories/IRecommendationRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChromaComics.Comics.Domain.Models;
+
+namespace ChromaComics.Comics.Domain.Repositories;
+
+public interface IRecommendationRepository
+{
+    Task<IEnumerable<Recommendation>> ListAsync();
+    Task AddAsync(Recommendation recommendation);
+    Task<Recommendation> FindByIdAsync(int id);
+    void Update(Recommendation recommendation);
+    void Remove(Recommendation recommendation);
+}
---
+++ ChromaComics/Recommendations/Domain/Services/Communication/RecommendationResponse.cs
@@ -0,0 +1,15 @@
+﻿using ChromaComics.Comics.Domain.Models;
+using ChromaComics.Shared.Domain.Services.Communication;
+
+namespace ChromaComics.Comics.Domain.Services.Communication;
+
+public class RecommendationResponse : BaseResponse<Recommendation>
+{
+    public RecommendationResponse(string message) : base(message)
+    {
+    }
+
+    public RecommendationResponse(Recommendation resource) : base(resource)
+    {
+    }
+}
