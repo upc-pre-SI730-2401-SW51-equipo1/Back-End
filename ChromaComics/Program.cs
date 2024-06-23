bf767ed35ececfe2f80a2bfb2f0a74bdacb8e208
Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>
2024-06-23 13:19:46 -0500

    Update Program.cs
    Signed-off-by: Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>

--- ChromaComics/Program.cs
+++ ChromaComics/Program.cs
@@ -1,7 +1,14 @@
+using ChromaComics.Comics.Mapping;
+using ChromaComics.Shared.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL());
+
+builder.Services.AddAutoMapper(typeof(ModelToResourceProfile), typeof(ResourceToModelProfile));
 
 // Add services to the container.
-
+    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
