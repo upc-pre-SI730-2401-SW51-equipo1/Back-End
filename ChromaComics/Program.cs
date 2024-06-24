af07a9bf8c6d7acc10b898508bd16890d0f5a3b0
UwUInspector <abcmanlol13@gmail.com>
2024-06-24 12:37:16 -0500

    program.cs fix

--- ChromaComics/Program.cs
+++ ChromaComics/Program.cs
@@ -1,9 +1,17 @@
+
+using ChromaComics.ShoppingCarts.Mapping;
+using ChromaComics.Shared.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL());
+
+builder.Services.AddAutoMapper(typeof(ModelToResourceProfile), typeof(ResourceToModelProfile));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
