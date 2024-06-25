752010fe24cac5c661c683a8c79aba5a04829748
Sergio Pecan <u20201a938@upc.edu.pe>
2024-06-25 14:10:54 -0500

    deploy

--- ChromaComics/Program.cs
+++ ChromaComics/Program.cs
@@ -1,26 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+
+ internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+                builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+                var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+                app.UseSwagger();
+                app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage(); // Página de excepciones para desarrolladores
-}
-app.UseHttpsRedirection();
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage(); // Página de excepciones para desarrolladores
+                }
+                app.UseHttpsRedirection();
 
-app.UseAuthorization();
+                app.UseAuthorization();
 
-app.MapControllers();
+                app.MapControllers();
 
-app.Run();
+                app.Run();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+    }
