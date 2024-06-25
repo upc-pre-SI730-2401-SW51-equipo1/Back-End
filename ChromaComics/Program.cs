256c1e7dd0ee0071b16cc4e9b67859b9060f0001
UwUInspector <abcmanlol13@gmail.com>
2024-06-25 18:48:02 -0500

    appsettings upd

--- ChromaComics/Program.cs
+++ ChromaComics/Program.cs
@@ -1,23 +1,34 @@
-
+using ChromaComics.ShoppingCarts.Domain.Repositories;
+using ChromaComics.ShoppingCarts.Domain.Services;
 using ChromaComics.ShoppingCarts.Mapping;
+using ChromaComics.ShoppingCarts.Services;
 using ChromaComics.Shared.Persistence.Contexts;
+using ChromaComics.Shared.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL());
 
+// Configurar DbContext con MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar AutoMapper
 builder.Services.AddAutoMapper(typeof(ModelToResourceProfile), typeof(ResourceToModelProfile));
 
-// Add services to the container.
+// Registrar servicios de aplicación y repositorios
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+
+// Agregar controladores
 builder.Services.AddControllers();
 
-
+// Agregar Endpoints API Explorer y Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de solicitudes HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- ChromaComics/obj/Debug/net8.0/ChromaComics.AssemblyInfo.cs
+++ ChromaComics/obj/Debug/net8.0/ChromaComics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChromaComics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195dabcdff26410a454e7a8c39451d865bdb63f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a790b6393463cbd796ec0aad2246250333be57")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChromaComics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChromaComics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
